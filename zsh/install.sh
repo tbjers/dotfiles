9a4c1669d0b1c959652cc54fde81f7b96d0a7978
Torgny Bjers <torgny@xorcode.com>
2015-06-06 18:06:01 -0400

    Change user's default shell to /bin/zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+echo "Changing default login shell to /bin/zsh..."
+chsh -s /bin/zsh
 curl -# https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > $DOTFILES/zsh/antigen.zsh
