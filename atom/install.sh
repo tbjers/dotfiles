8efd5a8bf000b572354ae1666dd1dd09a3ecb5c4
Torgny Bjers <torgny@xorcode.com>
2016-03-14 18:37:28 -0400

    Added Atom package installer

---
+++ atom/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apm install \
+  Stylus Zen atom-beautify atom-jade atom-wallaby bottom-dock case-conversion \
+  docblockr git-log gulp-manager language-docker language-markdown language-scala language-spacebars linter \
+  linter-docker linter-js-standard linter-jshint markdown-scroll-sync markdown-writer merge-conflicts \
+  minimap minimap-pigments octocat-syntax open-recent pigments project-manager react remote-sync sort-lines \
+  space-tab todo-show wordcount
