fbf79a8aa2e8162213bff496ac652cc7770e8cf3
Torgny Bjers <torgny@xorcode.com>
2016-03-17 23:46:24 -0400

    We need `ssh-copy-id` because it's so convenient

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -15,6 +15,6 @@ else
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark mc gibo tmux reattach-to-user-namespace direnv git-flow
+brew install grc coreutils spark mc gibo tmux reattach-to-user-namespace direnv git-flow ssh-copy-id
 
 exit 0
