91c802d38b4a1691ad2fb360c800fcf3fa79c086
Torgny Bjers <torgny@xorcode.com>
2016-05-16 21:46:19 -0400

    Installing a MySQL database to make life easier

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -15,6 +15,6 @@ else
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark mc gibo tmux reattach-to-user-namespace direnv git-flow ssh-copy-id openssl
+brew install grc coreutils spark mc gibo tmux reattach-to-user-namespace direnv git-flow ssh-copy-id openssl mariadb
 
 exit 0
