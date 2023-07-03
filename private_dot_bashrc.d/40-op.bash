1019aabcffcff022dd7e99f00ec323c6ff29fecf
Torgny Bjers <torgny@bjers.org>
2023-07-02 23:03:25 -0400

    update environment and add versions data file

--- private_dot_bashrc.d/40-op.bash
+++ private_dot_bashrc.d/40-op.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 if command -v op &>/dev/null; then
-  source <(op completion bash)
+    source <(op completion bash)
+    if [ -f "${HOME}/.config/op/plugins.sh" ]; then
+        source $HOME/.config/op/plugins.sh
+    fi
 fi
