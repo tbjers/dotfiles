38f8eb7c77193e433a83bcfc35d1b5190a20885b
Torgny Bjers <torgny@bjers.org>
2024-01-17 20:51:45 -0500

    feat: add brew bash completions

--- exact_private_dot_bashrc.d/40-brew.bash
+++ exact_private_dot_bashrc.d/40-brew.bash
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-if [ -d "/home/linuxbrew/.linuxbrew/bin" ]; then
-  source <(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+BREW_HOME="/home/linuxbrew/.linuxbrew"
+
+if [ -d "${BREW_HOME}/bin" ]; then
+  source <("${BREW_HOME}/bin/brew" shellenv)
+
+  if [ -d "${BREW_HOME}/etc/bash_completion.d" ]; then
+    for rc in "${BREW_HOME}/etc/bash_completion.d/"*; do
+      if [ -f "$rc" ]; then
+        # shellcheck disable=SC1090
+        source "$rc"
+      fi
+    done
+  fi
 fi
