993473bb6cab08448159914b20281d836b2ba82d
Torgny Bjers <torgny@bjers.org>
2024-03-01 18:40:48 -0500

    fix: adjust terminal settings to accomodate Warp

---
+++ exact_private_dot_bashrc.d/20-tabtab-completion.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ -f "${HOME}/.config/tabtab/bash/__tabtab.bash" ]; then
+  # shellcheck disable=SC1091
+  source "${HOME}/.config/tabtab/bash/__tabtab.bash"
+fi
--- exact_private_dot_bashrc.d/95-starship.bash
+++ exact_private_dot_bashrc.d/95-starship.bash
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-if command -v starship &>/dev/null; then
-  # shellcheck disable=SC1090
-  source <(starship init bash)
-  # shellcheck disable=SC1090
-  source <(starship completions bash)
+if [ "${EDITOR_TERMINAL}" != "WarpTerminal" ]; then
+  if command -v starship &>/dev/null; then
+    # shellcheck disable=SC1090
+    source <(starship init bash)
+    # shellcheck disable=SC1090
+    source <(starship completions bash)
+  fi
 fi
