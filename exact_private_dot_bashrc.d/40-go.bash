5b393a4abe03448c712afa46eba883803b774977
Torgny Bjers <torgny@bjers.org>
2024-03-28 03:02:47 -0400

    chore: add support for devboxes

--- exact_private_dot_bashrc.d/40-go.bash
+++ exact_private_dot_bashrc.d/40-go.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -d "${HOME}/.local/share/go" ]; then
+  PATH="${HOME}/.local/share/go/bin:${PATH}"
+fi
+
 if command -v go &>/dev/null; then
   PATH="${HOME}/go/bin:${PATH}"
 fi
