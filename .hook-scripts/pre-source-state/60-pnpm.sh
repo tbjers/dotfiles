03abbc05f6571d21c3a0b29500b745c52bb1cc8b
Torgny Bjers <torgny@bjers.org>
2024-03-28 03:15:07 -0400

    chore: improve pnpm installation

--- .hook-scripts/pre-source-state/60-pnpm.sh
+++ .hook-scripts/pre-source-state/60-pnpm.sh
@@ -10,7 +10,9 @@ source "${BASE_DIR}/script_utils.sh"
 
 case "$(osid)" in
 "fedora-silverblue")
+  # shellcheck disable=SC1090
   wget -qO- https://get.pnpm.io/install.sh | sh - &&
+    source ~/.local/share/chezmoi/exact_private_dot_bashrc.d/40-pnpm.bash &&
     pnpm env use --global iron
   ;;
 esac
