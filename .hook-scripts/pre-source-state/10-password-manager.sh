158e31bcc6e093d946cf2146115276f80f9d1bd0
Torgny Bjers <torgny@bjers.org>
2024-05-22 21:28:04 -0400

    feat: add support for Ubuntu

--- .hook-scripts/pre-source-state/10-password-manager.sh
+++ .hook-scripts/pre-source-state/10-password-manager.sh
@@ -25,6 +25,23 @@ case "$(osid)" in
   sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
   sudo dnf install -y -q 1password 1password-cli
   ;;
+"ubuntu")
+  if command -v distrobox-host-exec &>/dev/null; then
+    if [ ! -f "/usr/local/bin/op" ]; then
+      sudo ln -s "$(which distrobox-host-exec)" "/usr/local/bin/op"
+    fi
+  else
+    # shellcheck disable=SC2154
+    echo "${green}Installing 1Password${reset}"
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+    sudo apt update && sudo apt install -y -q 1password-cli
+  fi
+  ;;
 "wolfi")
   if command -v distrobox-host-exec &>/dev/null; then
     if [ ! -f "/usr/local/bin/op" ]; then
@@ -34,7 +51,7 @@ case "$(osid)" in
   ;;
 *)
   # shellcheck disable=SC2154
-  echo "${red}Unsupported OS${reset}"
+  echo "${red}Unsupported OS $(osid)${reset}"
   exit 0
   ;;
 esac
--- exact_private_dot_bashrc.d/40-pnpm.bash
+++ exact_private_dot_bashrc.d/40-pnpm.bash
@@ -6,3 +6,8 @@ case ":$PATH:" in
 *":$PNPM_HOME:"*) ;;
 *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
+
+if command -v pnpm &>/dev/null; then
+  # shellcheck disable=SC1090
+  source <(pnpm completion bash)
+fi
